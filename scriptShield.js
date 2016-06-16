ddd727b07d0bb0e892f31266dd85f4b930ed87b9
rotelstift <rotelstift@gmail.com>
2016-06-17 02:00:58 +0900

    ダメージリストを作りました。
    それと、グラフ全体を更新してあります。

--- scriptShield.js
+++ scriptShield.js
@@ -1,22 +1,31 @@
 window.onload = function () {
-	var damageRange = damageCal(1950); // 仮に2000度。本番では1000度に直す。
-	//alert(damageRange[0][0]);
+	var damageRange = damageCal(1700); // 仮に2000度。本番では1000度に直す。
+	alert(damageRange[1][0]);
 	var chartA = new CanvasJS.Chart("chartContainerA",
 	{
-		title:{
-			text: "My First Chart in CanvasJS"
-		},
+		// title:{
+		// 	text: "My First Chart in CanvasJS"
+		// },
 		axisY: {
 			includeZero:true,
 			title: "ダメージ値",
-			interval: 10,
+			interval: 30,
 			stripLines: [
-				{startValue: itemData[1][0] - damageRange[3][0],
-				endValue: itemData[1][1] - damageRange[3][1],
-			  opacity: .6},
-				{startValue: itemData[1][0],
-				endValue: itemData[1][1],
-        opacity: .2,}
+				{
+					startValue: itemData[1][0] - damageRange[3][1],
+					endValue: itemData[1][1] - damageRange[3][0],
+				  opacity: .4
+				},
+				{
+					startValue: itemData[1][0] - damageRange[3][0],
+					endValue: itemData[1][1] - damageRange[3][1],
+				  opacity: .8
+				},
+				{
+					startValue: itemData[1][0],
+					endValue: itemData[1][1],
+	        opacity: .2,
+				}
     ],
 		},
 		axisX: {
@@ -26,12 +35,12 @@ window.onload = function () {
 		data: [
 			{
 				type: "rangeBar",
-				showInLegend: true,
+				//showInLegend: true,
 				yValueFormatString: "#0.##",
 				indexLabel: "{y[#index]}",
 				dataPoints: [   // Y: [Low, High]
 					{x: 0, y:itemData[1], label: "成功ゾーン"},
-					{x: 0, y:[0, 30], label: "ダメージ総量"},
+					{x: 0, y:[0, 0], label: "ダメージ総量"},
 					{x: 10, y: [damageRange[1][0],damageRange[1][1]], label: "手加減打ち"},
 					{x: -10, y: [damageRange[2][0],damageRange[2][1]], label: "乱れ打ち"},
 					{x: 10, y: [damageRange[3][0],damageRange[3][1]], label: "叩く、狙い打ち"},
@@ -42,144 +51,238 @@ window.onload = function () {
 
 				]
 			}
-	/*
-		{
-			type: "rangeBar",
-			showInLegend: true,
-			yValueFormatString: "#0.##",
-			indexLabel: "{y[#index]}",
-			dataPoints: [   // Y: [Low, High]
-				{x: 0, y:itemData[1], label: "成功ゾーン"},
-			]
-		},{
-			type: "bar",
-			showInLegend: true,
-			dataPoints: [
-				{x: 0, y: 30, label: "ダメージ総量"}
-			]
-		},{
-			type: "rangeBar",
-			showInLegend: true,
-			dataPoints: [
-				{x: 10, y: [damageRange[1][0],damageRange[1][1]]},
-				//{x: -1, y: [damageRange[2][0],damageRange[2][1]]},
-				{x: 10, y: [damageRange[3][0],damageRange[3][1]]},
-			]
-		},{
-			type: "rangeBar",
-			showInLegend: true,
-			dataPoints: [
-				//{x: 1, y: [damageRange[1][0],damageRange[1][1]]},
-				{x: -9, y: [damageRange[2][0],damageRange[2][1]]},
-				//{x: 1, y: [damageRange[3][0],damageRange[3][1]]},
-			]
-		}
-			*/
 		]
-
 	});
 	var chartB = new CanvasJS.Chart("chartContainerB",
 	{
-		exportEnabled: true,
+		// title:{
+		// 	text: "My First Chart in CanvasJS"
+		// },
 		axisY: {
 			includeZero:true,
 			title: "ダメージ値",
-			interval: 10,
+			interval: 30,
+			stripLines: [
+				{
+					startValue: itemData[2][0] - damageRange[3][1],
+					endValue: itemData[2][1] - damageRange[3][0],
+				  opacity: .4
+				},
+				{
+					startValue: itemData[2][0] - damageRange[3][0],
+					endValue: itemData[2][1] - damageRange[3][1],
+				  opacity: .8
+				},
+				{
+					startValue: itemData[2][0],
+					endValue: itemData[2][1],
+	        opacity: .2,
+				}
+    ],
 		},
 		axisX: {
-			interval:10,
-			title: "Departments",
+			title: "ゲージ",
+			interval: 10000,
 		},
 		data: [
-		{
-			type: "rangeBar",
-			showInLegend: true,
-			yValueFormatString: "#0.##",
-			indexLabel: "{y[#index]}",
-			dataPoints: [   // Y: [Low, High]
-				{x: 10, y:itemData[1], label: "Data Scientist"},
-				{x: 20, y:itemData[2], label: "Product Manager" },
-				{x: 30, y:itemData[3], label: "Web Developer" },
-				{x: 40, y:itemData[4], label: "Software Engineer"},
-			]
-		}
+			{
+				type: "rangeBar",
+				//showInLegend: true,
+				yValueFormatString: "#0.##",
+				indexLabel: "{y[#index]}",
+				dataPoints: [   // Y: [Low, High]
+					{x: 0, y:itemData[2], label: "成功ゾーン"},
+					{x: 0, y:[0, 0], label: "ダメージ総量"},
+					{x: 10, y: [damageRange[1][0],damageRange[1][1]], label: "手加減打ち"},
+					{x: -10, y: [damageRange[2][0],damageRange[2][1]], label: "乱れ打ち"},
+					{x: 10, y: [damageRange[3][0],damageRange[3][1]], label: "叩く、狙い打ち"},
+					{x: -10, y: [damageRange[4][0],damageRange[4][1]], label: "上下打ち、4連打ち、ななめ打ち"},
+					{x: 10, y: [damageRange[5][0],damageRange[5][1]], label: "2倍打ち、超4連打ち"},
+					{x: -10, y: [damageRange[6][0],damageRange[6][1]], label: "熱風おろし"},
+					{x: 10, y: [damageRange[7][0],damageRange[7][1]], label: "3倍打ち"}
+
+				]
+			}
 		]
 	});
 	var chartC = new CanvasJS.Chart("chartContainerC",
 	{
-		exportEnabled: true,
+		// title:{
+		// 	text: "My First Chart in CanvasJS"
+		// },
 		axisY: {
 			includeZero:true,
 			title: "ダメージ値",
-			interval: 10,
+			interval: 30,
+			stripLines: [
+				{
+					startValue: itemData[3][0] - damageRange[3][1],
+					endValue: itemData[3][1] - damageRange[3][0],
+					opacity: .4
+				},
+				{
+					startValue: itemData[3][0] - damageRange[3][0],
+					endValue: itemData[3][1] - damageRange[3][1],
+					opacity: .8
+				},
+				{
+					startValue: itemData[3][0],
+					endValue: itemData[3][1],
+					opacity: .2,
+				}
+		],
 		},
 		axisX: {
-			interval:10,
-			title: "Departments",
+			title: "ゲージ",
+			interval: 10000,
 		},
 		data: [
-		{
-			type: "rangeBar",
-			showInLegend: true,
-			yValueFormatString: "#0.##",
-			indexLabel: "{y[#index]}",
-			dataPoints: [   // Y: [Low, High]
-				{x: 10, y:itemData[1], label: "Data Scientist"},
-				{x: 20, y:itemData[2], label: "Product Manager" },
-				{x: 30, y:itemData[3], label: "Web Developer" },
-				{x: 40, y:itemData[4], label: "Software Engineer"},
-			]
-		}
+			{
+				type: "rangeBar",
+				//showInLegend: true,
+				yValueFormatString: "#0.##",
+				indexLabel: "{y[#index]}",
+				dataPoints: [   // Y: [Low, High]
+					{x: 0, y:itemData[3], label: "成功ゾーン"},
+					{x: 0, y:[0, 0], label: "ダメージ総量"},
+					{x: 10, y: [damageRange[1][0],damageRange[1][1]], label: "手加減打ち"},
+					{x: -10, y: [damageRange[2][0],damageRange[2][1]], label: "乱れ打ち"},
+					{x: 10, y: [damageRange[3][0],damageRange[3][1]], label: "叩く、狙い打ち"},
+					{x: -10, y: [damageRange[4][0],damageRange[4][1]], label: "上下打ち、4連打ち、ななめ打ち"},
+					{x: 10, y: [damageRange[5][0],damageRange[5][1]], label: "2倍打ち、超4連打ち"},
+					{x: -10, y: [damageRange[6][0],damageRange[6][1]], label: "熱風おろし"},
+					{x: 10, y: [damageRange[7][0],damageRange[7][1]], label: "3倍打ち"}
+
+				]
+			}
 		]
 	});
 	var chartD = new CanvasJS.Chart("chartContainerD",
 	{
-		exportEnabled: true,
+		// title:{
+		// 	text: "My First Chart in CanvasJS"
+		// },
 		axisY: {
 			includeZero:true,
 			title: "ダメージ値",
-			interval: 10,
+			interval: 30,
+			stripLines: [
+				{
+					startValue: itemData[4][0] - damageRange[3][1],
+					endValue: itemData[4][1] - damageRange[3][0],
+					opacity: .4
+				},
+				{
+					startValue: itemData[4][0] - damageRange[3][0],
+					endValue: itemData[4][1] - damageRange[3][1],
+					opacity: .8
+				},
+				{
+					startValue: itemData[4][0],
+					endValue: itemData[4][1],
+					opacity: .2,
+				}
+		],
 		},
 		axisX: {
-			interval:10,
-			title: "Departments",
+			title: "ゲージ",
+			interval: 10000,
 		},
 		data: [
-		{
-			type: "rangeBar",
-			showInLegend: true,
-			yValueFormatString: "#0.##",
-			indexLabel: "{y[#index]}",
-			dataPoints: [   // Y: [Low, High]
-				{x: 10, y:itemData[1], label: "Data Scientist"},
-				{x: 20, y:itemData[2], label: "Product Manager" },
-				{x: 30, y:itemData[3], label: "Web Developer" },
-				{x: 40, y:itemData[4], label: "Software Engineer"},
-			]
-		}
+			{
+				type: "rangeBar",
+				//showInLegend: true,
+				yValueFormatString: "#0.##",
+				indexLabel: "{y[#index]}",
+				dataPoints: [   // Y: [Low, High]
+					{x: 0, y:itemData[4], label: "成功ゾーン"},
+					{x: 0, y:[0, 0], label: "ダメージ総量"},
+					{x: 10, y: [damageRange[1][0],damageRange[1][1]], label: "手加減打ち"},
+					{x: -10, y: [damageRange[2][0],damageRange[2][1]], label: "乱れ打ち"},
+					{x: 10, y: [damageRange[3][0],damageRange[3][1]], label: "叩く、狙い打ち"},
+					{x: -10, y: [damageRange[4][0],damageRange[4][1]], label: "上下打ち、4連打ち、ななめ打ち"},
+					{x: 10, y: [damageRange[5][0],damageRange[5][1]], label: "2倍打ち、超4連打ち"},
+					{x: -10, y: [damageRange[6][0],damageRange[6][1]], label: "熱風おろし"},
+					{x: 10, y: [damageRange[7][0],damageRange[7][1]], label: "3倍打ち"}
+
+				]
+			}
 		]
 	});
+
+
 	chartA.render();
 	chartB.render();
 	chartC.render();
 	chartD.render();
 
+	function refreshChartA(){
+		//var length = chart.options.data[0].dataPoints.length;
+		var damage = parseInt($("#damageA").val());
+		//alert(damage);
+		//alert("hoge");
+		//alert(damageCal(1950));
+		var temp = parseInt($("#temperature").val());
+		damageRange = damageCal(temp);
+		//alert(damageRange);
+		chartA.options.data[0].dataPoints[1].y[1] = damage;
+		for(var i = 2; i <= 8; i++){
+			chartA.options.data[0].dataPoints[i].y = [damageRange[i - 1][0] + damage, damageRange[i - 1][1] + damage];
+		}
+		chartA.render();
+	}
+	function refreshChartB(){
+		//var length = chart.options.data[0].dataPoints.length;
+		var damage = parseInt($("#damageB").val());
+		//alert(damage);
+		//alert("hoge");
+		//alert(damageCal(1950));
+		var temp = parseInt($("#temperature").val());
+		damageRange = damageCal(temp);
+		//alert(damageRange);
+		chartB.options.data[0].dataPoints[1].y[1] = damage;
+		for(var i = 2; i <= 8; i++){
+			chartB.options.data[0].dataPoints[i].y = [damageRange[i - 1][0] + damage, damageRange[i - 1][1] + damage];
+		}
+		chartB.render();
+	}
+	function refreshChartC(){
+		//var length = chart.options.data[0].dataPoints.length;
+		var damage = parseInt($("#damageC").val());
+		//alert(damage);
+		//alert("hoge");
+		//alert(damageCal(1950));
+		var temp = parseInt($("#temperature").val());
+		damageRange = damageCal(temp);
+		//alert(damageRange);
+		chartC.options.data[0].dataPoints[1].y[1] = damage;
+		for(var i = 2; i <= 8; i++){
+			chartC.options.data[0].dataPoints[i].y = [damageRange[i - 1][0] + damage, damageRange[i - 1][1] + damage];
+		}
+		chartC.render();
+	}
+	function refreshChartD(){
+		//var length = chart.options.data[0].dataPoints.length;
+		var damage = parseInt($("#damageD").val());
+		//alert(damage);
+		//alert("hoge");
+		//alert(damageCal(1950));
+		var temp = parseInt($("#temperature").val());
+		damageRange = damageCal(temp);
+		//alert(damageRange);
+		chartD.options.data[0].dataPoints[1].y[1] = damage;
+		for(var i = 2; i <= 8; i++){
+			chartD.options.data[0].dataPoints[i].y = [damageRange[i - 1][0] + damage, damageRange[i - 1][1] + damage];
+		}
+		chartD.render();
+	}
+
 	$("#refresh").click(function () {
 
-	//var length = chart.options.data[0].dataPoints.length;
-	var damage = parseInt($("#damageA").val());
-	//alert(damage);
-	//alert("hoge");
-	//alert(damageCal(1950));
-	var temperature = $("#temperature").val();
-	damageRange = damageCal(temp);
-	//alert(damageRange);
-	chartA.options.data[0].dataPoints[1].y[1] = damage;
-	for(var i = 2; i <= 8; i++){
-		chartA.options.data[0].dataPoints[i].y[0] += damage;
-		chartA.options.data[0].dataPoints[i].y[1] += damage;
-	}
-	chartA.render();
+	refreshChartA();
+	refreshChartB();
+	refreshChartC();
+	refreshChartD();
 
 	});
 
